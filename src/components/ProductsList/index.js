21dedeada169c5162a9d45c2f20da306f9e58a56
ProgramistaiJa <programista@i-ja.pl>
2018-02-17 12:33:19 +0100

    Added Table from Material UI

--- src/components/ProductsList/index.js
+++ src/components/ProductsList/index.js
@@ -1,173 +1,32 @@
 import React from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Table, {
     TableBody,
     TableCell,
     TableFooter,
-    TableHead,
     TablePagination,
     TableRow,
-    TableSortLabel,
 } from 'material-ui/Table';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
 import Paper from 'material-ui/Paper';
 import Checkbox from 'material-ui/Checkbox';
-import IconButton from 'material-ui/IconButton';
-import Tooltip from 'material-ui/Tooltip';
-import DeleteIcon from 'material-ui-icons/Delete';
-import FilterListIcon from 'material-ui-icons/FilterList';
-import { lighten } from 'material-ui/styles/colorManipulator';
-import EnhancedTableToolbar from './EnhancedTableToolbar'
+import EnhancedTableToolbar from './EnhancedTableToolbar';
+import EnhancedTableHead from './EnhancedTableHead';
+import Button from 'material-ui/Button';
 
 let counter = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(name, units, price/*, fat, carbs, protein*/) {
     counter += 1;
-    return { id: counter, name, calories, fat, carbs, protein };
+    return { id: counter, name, units, price/*, fat, carbs, protein*/};
 }
-
-const columnData = [
-    { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-    { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-    { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-    { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-    { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
-];
-
-class EnhancedTableHead extends React.Component {
-    createSortHandler = property => event => {
-        this.props.onRequestSort(event, property);
-    };
-
-    render() {
-        const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
-
-        return (
-            <TableHead>
-                <TableRow>
-                    <TableCell padding="checkbox">
-                        <Checkbox
-                            indeterminate={numSelected > 0 && numSelected < rowCount}
-                            checked={numSelected === rowCount}
-                            onChange={onSelectAllClick}
-                        />
-                    </TableCell>
-                    {columnData.map(column => {
-                        return (
-                            <TableCell
-                                key={column.id}
-                                numeric={column.numeric}
-                                padding={column.disablePadding ? 'none' : 'default'}
-                                sortDirection={orderBy === column.id ? order : false}
-                            >
-                                <Tooltip
-                                    title="Sort"
-                                    placement={column.numeric ? 'bottom-end' : 'bottom-start'}
-                                    enterDelay={300}
-                                >
-                                    <TableSortLabel
-                                        active={orderBy === column.id}
-                                        direction={order}
-                                        onClick={this.createSortHandler(column.id)}
-                                    >
-                                        {column.label}
-                                    </TableSortLabel>
-                                </Tooltip>
-                            </TableCell>
-                        );
-                    }, this)}
-                </TableRow>
-            </TableHead>
-        );
-    }
-}
-
-EnhancedTableHead.propTypes = {
-    numSelected: PropTypes.number.isRequired,
-    onRequestSort: PropTypes.func.isRequired,
-    onSelectAllClick: PropTypes.func.isRequired,
-    order: PropTypes.string.isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired,
-};
-
-// const toolbarStyles = theme => ({
-//     root: {
-//         paddingRight: theme.spacing.unit,
-//     },
-//     highlight:
-//         theme.palette.type === 'light'
-//             ? {
-//                 color: theme.palette.secondary.dark,
-//                 backgroundColor: lighten(theme.palette.secondary.light, 0.4),
-//             }
-//             : {
-//                 color: lighten(theme.palette.secondary.light, 0.4),
-//                 backgroundColor: theme.palette.secondary.dark,
-//             },
-//     spacer: {
-//         flex: '1 1 100%',
-//     },
-//     actions: {
-//         color: theme.palette.text.secondary,
-//     },
-//     title: {
-//         flex: '0 0 auto',
-//     },
-// });
-//
-// let EnhancedTableToolbar = props => {
-//     const { numSelected, classes } = props;
-//
-//     return (
-//         <Toolbar
-//             className={classNames(classes.root, {
-//                 [classes.highlight]: numSelected > 0,
-//             })}
-//         >
-//             <div className={classes.title}>
-//                 {numSelected > 0 ? (
-//                     <Typography variant="subheading">{numSelected} selected</Typography>
-//                 ) : (
-//                     <Typography variant="title">Nutrition</Typography>
-//                 )}
-//             </div>
-//             <div className={classes.spacer} />
-//             <div className={classes.actions}>
-//                 {numSelected > 0 ? (
-//                     <Tooltip title="Delete">
-//                         <IconButton aria-label="Delete">
-//                             <DeleteIcon />
-//                         </IconButton>
-//                     </Tooltip>
-//                 ) : (
-//                     <Tooltip title="Filter list">
-//                         <IconButton aria-label="Filter list">
-//                             <FilterListIcon />
-//                         </IconButton>
-//                     </Tooltip>
-//                 )}
-//             </div>
-//         </Toolbar>
-//     );
-// };
-//
-// EnhancedTableToolbar.propTypes = {
-//     classes: PropTypes.object.isRequired,
-//     numSelected: PropTypes.number.isRequired,
-// };
-//
-// EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
 
 const styles = theme => ({
     root: {
-        width: '100%',
+        width: '100vw',
         marginTop: theme.spacing.unit * 3,
     },
     table: {
-        minWidth: 800,
+        minWidth: 300,
     },
     tableWrapper: {
         overflowX: 'auto',
@@ -180,23 +39,23 @@ class EnhancedTable extends React.Component {
 
         this.state = {
             order: 'asc',
-            orderBy: 'calories',
+            orderBy: 'name',
             selected: [],
             data: [
-                createData('Cupcake', 305, 3.7, 67, 4.3),
-                createData('Donut', 452, 25.0, 51, 4.9),
-                createData('Eclair', 262, 16.0, 24, 6.0),
-                createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-                createData('Gingerbread', 356, 16.0, 49, 3.9),
-                createData('Honeycomb', 408, 3.2, 87, 6.5),
-                createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-                createData('Jelly Bean', 375, 0.0, 94, 0.0),
-                createData('KitKat', 518, 26.0, 65, 7.0),
-                createData('Lollipop', 392, 0.2, 98, 0.0),
-                createData('Marshmallow', 318, 0, 81, 2.0),
-                createData('Nougat', 360, 19.0, 9, 37.0),
-                createData('Oreo', 437, 18.0, 63, 4.0),
-            ].sort((a, b) => (a.calories < b.calories ? -1 : 1)),
+                createData('Jabłka', 1, 305, 3.7, 67, 4.3),
+                createData('Pomarańcze', 2, 452, 25.0, 51, 4.9),
+                createData('Kiwi', 3, 262, 16.0, 24, 6.0),
+                createData('Granaty', 4, 159, 6.0, 24, 4.0),
+                createData('Banany', 5, 356, 16.0, 49, 3.9),
+                createData('Pomidory', 6, 408, 3.2, 87, 6.5),
+                createData('Ogórki', 7, 237, 9.0, 37, 4.3),
+                createData('Sałata', 8, 375, 0.0, 94, 0.0),
+                createData('KitKat', 9, 518, 26.0, 65, 7.0),
+                createData('Lollipop', 10, 392, 0.2, 98, 0.0),
+                createData('Marshmallow', 11, 318, 0, 81, 2.0),
+                createData('Nougat', 12, 360, 19.0, 9, 37.0),
+                createData('Oreo', 13, 437, 18.0, 63, 4.0),
+            ].sort((a, b) => (a.name < b.name ? -1 : 1)),
             page: 0,
             rowsPerPage: 5,
         };
@@ -292,10 +151,11 @@ class EnhancedTable extends React.Component {
                                             <Checkbox checked={isSelected} />
                                         </TableCell>
                                         <TableCell padding="none">{n.name}</TableCell>
-                                        <TableCell numeric>{n.calories}</TableCell>
-                                        <TableCell numeric>{n.fat}</TableCell>
-                                        <TableCell numeric>{n.carbs}</TableCell>
-                                        <TableCell numeric>{n.protein}</TableCell>
+                                        <TableCell numeric>{n.units}</TableCell>
+                                        <TableCell numeric>{n.price}</TableCell>
+                                        {/*<TableCell numeric>{n.fat}</TableCell>*/}
+                                        {/*<TableCell numeric>{n.carbs}</TableCell>*/}
+                                        {/*<TableCell numeric>{n.protein}</TableCell>*/}
                                     </TableRow>
                                 );
                             })}
@@ -325,6 +185,10 @@ class EnhancedTable extends React.Component {
                         </TableFooter>
                     </Table>
                 </div>
+                <div>cena: SUMA</div>
+                <div> <Button variant="raised" color="primary" className={classes.button}>
+                    Formularz dostawy
+                </Button></div>
             </Paper>
         );
     }
